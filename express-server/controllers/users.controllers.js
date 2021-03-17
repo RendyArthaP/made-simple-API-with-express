084bd6222d31301e6fd0c9d0dc3cdb616bf29035
Rendy Artha <rendyartha312@gmail.com>
2021-03-17 14:12:10 +0700

    made controller for user routes

---
+++ express-server/controllers/users.controllers.js
@@ -0,0 +1,45 @@
+const users = require('../models/users');
+
+module.exports = {
+  getUser: (req, res) => {
+    res.json({
+      message: "get users success",
+      data: users
+    })
+  },
+  getUserByID: (req, res) => {
+    const id = req.params.id
+    const user = users.find(item => item.id == id)
+  
+    if(!user) {
+      res.json({
+        message: "data id tidak ditemukan"
+      })
+    } else {
+      res.json({
+        message: "get users by id success",
+        data: user
+      })
+    }
+  
+  },
+  addUser: (req, res) => {
+    const user = req.body
+    users.push(user)
+  
+    res.json({
+      message: "success adding new user",
+      data: users
+    })
+  },
+  deleteUser: (req, res) => {
+    const id = req.params.id
+    const user = users.filter(item => item.id != id)
+  
+    res.json({
+      message: "users deleted success",
+      data: user
+    })
+  
+  }
+}
--- express-server/routes/user.routes.js
+++ express-server/routes/user.routes.js
@@ -1,50 +1,19 @@
 const express = require('express');
 const users = require('../models/users');
 const router = express.Router()
+const { 
+  getUser, 
+  getUserByID, 
+  addUser, 
+  deleteUser 
+} = require('../controllers/users.controllers');
 
-router.get('/', (req, res) => {
-  res.json({
-    message: "get users success",
-    data: users
-  })
-})
+router.get('/', getUser)
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id
-  const user = users.find(item => item.id == id)
+router.get('/:id', getUserByID)
 
-  if(!user) {
-    res.json({
-      message: "data id tidak ditemukan"
-    })
-  } else {
-    res.json({
-      message: "get users by id success",
-      data: user
-    })
-  }
+router.post('/', addUser)
 
-})
-
-router.post('/', (req, res) => {
-  const user = req.body
-  users.push(user)
-
-  res.json({
-    message: "success adding new user",
-    data: users
-  })
-})
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  const user = users.filter(item => item.id != id)
-
-  res.json({
-    message: "users deleted success",
-    data: user
-  })
-
-})
+router.delete('/:id', deleteUser)
 
 module.exports = router
