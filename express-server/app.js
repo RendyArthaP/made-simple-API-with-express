3e9fb459f2c6642b481369aecce520230a75e2d3
Rendy Artha <rendyartha312@gmail.com>
2021-03-17 11:20:57 +0700

    get users api

--- express-server/app.js
+++ express-server/app.js
@@ -3,9 +3,31 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+const users = [
+  {
+    id: 0,
+    nama: "rendy"
+  },
+  {
+    id: 1,
+    nama: "artha"
+  },
+  {
+    id: 2,
+    nama: "prawira"
+  },
+]
+
 app.get('/', (req, res) => {
   res.json({
     message: "Hello everyone!"
+  })
+})
+
+app.get('/users', (req, res) => {
+  res.json({
+    message: "get users success",
+    data: users
   })
 })
 
