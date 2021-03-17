83cb6c170c68e25e24f09450a64b4961ce8c8c5c
Rendy Artha <rendyartha312@gmail.com>
2021-03-17 11:19:29 +0700

    get message with api

--- express-server/app.js
+++ express-server/app.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+app.get('/', (req, res) => {
+  res.json({
+    message: "Hello everyone!"
+  })
+})
+
 app.listen(port, () => {
   console.log(`example app at http://localhost:${port}`)
 })
